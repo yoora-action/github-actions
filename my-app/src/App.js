296514005441ebe9d0b73424aa66154e2bef1f5d
yoora <espoir3045@gmail.com>
2025-02-14 15:41:15 +0900

    fix

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          ffkkfkfkfkfkfk
+          다~~~~~~~~~~~~~시
         </a>
       </header>
     </div>
